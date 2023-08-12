a3edfc209c6d0ed4477ad2fbba15e0a69c2ac60a
aravindsaipa-kore <aravindsai.panasa@kore.com>
2023-08-12 12:31:58 +0530

    Learn JPA Hibernete JDBC folder created

---
+++ Spring-Data-JPA/Learning JPA HIBERNETE JDBC/learn-JPA-and-Hibernete/src/main/java/com/springboot/learnJPAandHibernete/LearnJpaAndHiberneteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.learnJPAandHibernete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHiberneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHiberneteApplication.class, args);
+	}
+
+}
