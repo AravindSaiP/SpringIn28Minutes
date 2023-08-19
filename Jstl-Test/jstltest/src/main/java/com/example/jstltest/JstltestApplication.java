16d89f098f8ae4386d32c541bcc360c07e2162c0
aravindsaipa-kore <aravindsai.panasa@kore.com>
2023-08-19 23:54:21 +0530

    Before impelemting JSPF folders

---
+++ Jstl-Test/jstltest/src/main/java/com/example/jstltest/JstltestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jstltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstltestApplication.class, args);
+	}
+
+}
